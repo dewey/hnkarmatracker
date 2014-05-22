c49a3d76494a3fdf6040d76e8bfefaf3054b1fca
dewey <mail@notmyhostna.me>
2014-05-22 19:49:41 +0200

    Store the number of current subscribers and a timestamp to a redis list.

--- populate.js
+++ populate.js
@@ -11,14 +11,14 @@ var config = require('./package.json'),
     hn = request.newClient('https://hn.algolia.com/api/v1/'),
     async = require('async');
 
-// Setting up the timestamps so we only search for submissions in the last 24h.
-var now = moment().unix();
-var oneDayAgo = (moment().subtract('hours', 24)).unix();
-
 // This API call will grab all the authors of story submissions submitted in the last 24h
 // with more than 5 points. These are added to our redis db hntracker-users and hntracker-<username>
 // the field created will be set to the current timestamp
 new cron('0 0 * * * *', function() {
+    // Setting up the timestamps so we only search for submissions in the last 24h.
+    var now = moment().unix();
+    var oneDayAgo = (moment().subtract('hours', 24)).unix();
+
     hn.get("search_by_date?tags=story&numericFilters=created_at_i>" + oneDayAgo + ",created_at_i<" + now + ",points>5", function(err, response, body) {
         if (!err) {
             var pages = body['nbPages'];
@@ -49,4 +49,62 @@ new cron('0 0 * * * *', function() {
             console.log("Error fetching number of pages from the API.")
         }
     });
+}, null, true, "America/Los_Angeles");
+
+// This function is used to get the number of members in the <redisprefix>-users set.
+function getNumberOfMembers(redisKey, cb) {
+    client.scard(redisKey, function(err, data) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, data);
+    })
+}
+
+// This function will pop old datapoints from our list
+function purgeOldDatapoint(redisKey, cb) {
+    client.llen(redisKey, function(err, res) {
+        if (err) {
+            return cb(err);
+        }
+        if (res >= 10) {
+            return client.lpop(redisKey, function(err, res) {
+                if (err) {
+                    return cb(err)
+                }
+                cb();
+            })
+        }
+        cb();
+    })
+}
+
+// This cronjob will add the current number of tracked users and a timestamp to a list called <redisprefix>-users-stats. This
+// will allow us to have a simple growth chart.
+new cron('* * * * * *', function() {
+    var now = moment().unix();
+    async.series([
+
+            // Getting the number of members
+            function(callback) {
+                getNumberOfMembers(config.app.redis.prefix + "-users", callback)
+            },
+            function(callback) {
+                purgeOldDatapoint(config.app.redis.prefix + "-users-stats", callback)
+            }
+        ],
+        // Once the number of members is retrieved and the old datapoints removed
+        function(err, results) {
+            if (!err) {
+                // This will add a new datapoint to the list. The list will contain keys looking like: timestamp:NumberOfMembers
+                // and will help us to keep track on how many users we were tracking every day for some basic statistics and to
+                // display the number on the front page.
+                client.rpush(config.app.redis.prefix + "-users-stats", now + ":" + results[0], function(err, res) {
+                    if (err) {
+                        console.log("Error pushing value to stats");
+                    }
+                })
+            }
+        }
+    )
 }, null, true, "America/Los_Angeles");
