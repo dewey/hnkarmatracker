49b37799ba7f66d8accc0d2883ae27dfbc2bb703
dewey <mail@notmyhostna.me>
2014-06-09 23:05:17 +0200

    Added fonts and fixed tracking JS

--- public/javascripts/gauges.js
+++ public/javascripts/gauges.js
@@ -1,13 +1,11 @@
-<script type="text/javascript">
-  var _gauges = _gauges || [];
-  (function() {
-    var t   = document.createElement('script');
-    t.type  = 'text/javascript';
-    t.async = true;
-    t.id    = 'gauges-tracker';
-    t.setAttribute('data-site-id', '5396157ceddd5b053e0070eb');
-    t.src = '//secure.gaug.es/track.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(t, s);
-  })();
-</script>
+var _gauges = _gauges || [];
+(function() {
+  var t   = document.createElement('script');
+  t.type  = 'text/javascript';
+  t.async = true;
+  t.id    = 'gauges-tracker';
+  t.setAttribute('data-site-id', '5396157ceddd5b053e0070eb');
+  t.src = '//secure.gaug.es/track.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(t, s);
+})();
