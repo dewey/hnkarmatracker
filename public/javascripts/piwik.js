fbad185e2cfb4a44a95fabe78b80086f70b79dd5
dewey <mail@notmyhostna.me>
2014-12-15 12:21:56 +0100

    Removed gaug.es tracking code.

---
+++ public/javascripts/piwik.js
@@ -0,0 +1,13 @@
+<!-- Piwik -->
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+var u="//analytics.notmyhostna.me/";
+_paq.push(['setTrackerUrl', u+'piwik.php']);
+_paq.push(['setSiteId', 6]);
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+<noscript><p><img src="//analytics.notmyhostna.me/piwik.php?idsite=6" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
