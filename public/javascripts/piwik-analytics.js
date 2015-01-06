95a6adcbe628432143e60a386b88ab4a91b01361
dewey <mail@notmyhostna.me>
2015-01-06 12:54:00 +0100

    Fix

--- public/javascripts/piwik-analytics.js
+++ public/javascripts/piwik-analytics.js
@@ -1,4 +1,3 @@
-<!-- Piwik -->
 var _paq = _paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
@@ -9,5 +8,3 @@ _paq.push(['setSiteId', 5]);
 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 })();
-<noscript><p><img src="//analytics.notmyhostna.me/piwik.php?idsite=5" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
