046c0aaea8f6f1bee7f9d629a133eb370f27a02a
dewey <mail@notmyhostna.me>
2015-01-06 12:32:36 +0100

    Updated Piwik ID

--- public/javascripts/piwik.js
+++ public/javascripts/piwik.js
@@ -5,9 +5,9 @@ _paq.push(['enableLinkTracking']);
 (function() {
 var u="//analytics.notmyhostna.me/";
 _paq.push(['setTrackerUrl', u+'piwik.php']);
-_paq.push(['setSiteId', 6]);
+_paq.push(['setSiteId', 5]);
 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 })();
-<noscript><p><img src="//analytics.notmyhostna.me/piwik.php?idsite=6" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="//analytics.notmyhostna.me/piwik.php?idsite=5" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
